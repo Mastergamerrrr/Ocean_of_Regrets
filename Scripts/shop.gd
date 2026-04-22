23bf1b3c7d1b8da374128f3780d8f3902d6a8761
p-joshua09 <joshuapotot09@gmail.com>
2026-04-22 14:48:40 +0800

    Deleted the old ui shop and implemented new ui for shop

--- Scripts/shop.gd
+++ Scripts/shop.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @onready var interactable: Area2D = $Interactable
-@onready var shop_ui = $ShopUI  # We'll create this as a CanvasLayer
+# Remove @onready var shop_ui = $ShopUI  # We'll instantiate dynamically
 
 func _ready() -> void:
 	interactable.interact_name = "Shop"
@@ -9,5 +9,22 @@ func _ready() -> void:
 		await open_shop()
 
 func open_shop() -> void:
+	# Load the shop UI scene
+	var shop_scene = load("res://Scenes/mainshopUI.tscn")
+	var shop_ui = shop_scene.instantiate()
+	
+	# Add to current scene (CanvasLayer will render on top automatically)
+	get_tree().current_scene.add_child(shop_ui)
+	
+	# Pause the game world while shop is open (optional)
+	get_tree().paused = true
+	
+	# Show the shop (if you have an animation or initial setup)
 	shop_ui.show_shop()
+	
+	# Wait for the shop to emit its closed signal
 	await shop_ui.shop_closed
+	
+	# Cleanup
+	get_tree().paused = false
+	shop_ui.queue_free()
