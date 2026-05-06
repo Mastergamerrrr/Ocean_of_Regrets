fc89ab613f79a5a35f1ba40d2b083f886b1f2322
KainaP00P <mikzarate33@gmail.com>
2026-05-07 02:46:44 +0800

    Rename transition assets; add sound and slow tween
    Rename transition shader and script files (transition_effect.gdshader -> IrisWipe.gdshader, transition_effect.gd -> TransitionEffect.gd) and update scene. Add an AudioStream (Spongebob Bubble Transition.mp3) and an AudioStreamPlayer node to the TransitionEffect.tscn, wire it into the script via a new onready var, and play the sound when closing and opening the iris. Increase the tween close duration from 0.5s to 1.5s for a slower transition. Remove old TP_sound assets and obsolete transition_* uid/tscn files; add new UID files for the renamed resources.

--- Scenes/TransitionEffect.gd
+++ Scenes/TransitionEffect.gd
@@ -1,24 +1,26 @@
 extends CanvasLayer
 
 @onready var color_rect = $ColorRect
+@onready var transition_sound = $TransitionSound
 var tween: Tween
 
 func _ready():
 	_set_progress(1.0)
 
 func transition_to(target_scene: String):
-	# Kill any existing tween first
 	if tween:
 		tween.kill()
 	
-	# Iris close
+	# Play sound and close iris
+	transition_sound.play()
 	tween = create_tween()
-	tween.tween_method(_set_progress, 1.0, 0.0, 0.5)
+	tween.tween_method(_set_progress, 1.0, 0.0, 1.5)
 	await tween.finished
 	
 	get_tree().change_scene_to_file(target_scene)
 	
-	# Iris open
+	# Play sound and open iris
+	transition_sound.play()
 	if tween:
 		tween.kill()
 	tween = create_tween()
