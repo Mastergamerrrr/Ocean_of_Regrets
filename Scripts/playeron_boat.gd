fec88fa938a099d857a2da4ae1c7c4f3bc04e004
Mastergamerrrr <labortejian5@gmail.com>
2026-05-12 14:15:30 +0800

    Revert "Merge branch 'KimTest' into Joshmaxmerge"
    This reverts commit 21cac08fe26495bf7b3a547ca67167ae954ecbbc, reversing
    changes made to 9f43ffadd16a8a98a70738cab8fe5391ee9d83fc.

--- Scripts/playeron_boat.gd
+++ Scripts/playeron_boat.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+
 const SPEED = 70.0
 const JUMP_VELOCITY = -200.0
 
@@ -11,10 +12,6 @@ var current_water_type: String = ""
 
 @onready var animated_spriteboat: AnimatedSprite2D = $BOAT/PLAYERBOATAnim
 @onready var spriteb: Sprite2D = $BOAT
-@onready var row_sound: AudioStreamPlayer2D = $RowSound
-
-var footstep_timer: float = 0.0
-const ROW_INTERVAL: float = 8.91
 
 
 signal fish_caught
@@ -53,16 +50,9 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction * SPEED
 		animated_spriteboat.play("Row")
-		if is_on_floor():
-			footstep_timer -= delta
-			if footstep_timer <= 0.0:
-				row_sound.play()
-				footstep_timer = ROW_INTERVAL
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		animated_spriteboat.play("Idle")
-		footstep_timer = 0.0
-		row_sound.stop()
 
 	move_and_slide()
 
--- stone_zone.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-func _on_body_entered(body: Node2D) -> void:
-	if body.has_method("set_surface"):
-		body.set_surface("stone")
-
-func _on_body_exited(body: Node2D) -> void:
-	if body.has_method("set_surface"):
-		body.set_surface("grass")
