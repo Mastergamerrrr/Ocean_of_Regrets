fde8cfc053a75a131cc1a5f2c98e7e354d3f1fb0
Mastergamerrrr <labortejian5@gmail.com>
2026-05-13 15:44:07 +0800

    fixed deleted credits

--- Scenes/v_box_container.gd
+++ Scenes/v_box_container.gd
@@ -2,15 +2,12 @@ extends VBoxContainer
 
 @onready var start_button: Button = $start
 @onready var options_button: Button = $options
-@onready var credits_button: Button = $credits
 
 func _ready():
 	if start_button:
 		start_button.pressed.connect(_on_start_pressed)
 	if options_button:
 		options_button.pressed.connect(_on_options_pressed)
-	if credits_button:
-		credits_button.pressed.connect(_on_credits_pressed)
 
 func _on_start_pressed():
 	# Change to the game scene
@@ -19,8 +16,3 @@ func _on_start_pressed():
 func _on_options_pressed():
 	# Change to the options scene
 	get_tree().change_scene_to_file("res://Scenes/options.tscn")
-	
-
-func _on_credits_pressed():
-	# Change to the credits scene
-	get_tree().change_scene_to_file("res://Scenes/credits.tscn")
