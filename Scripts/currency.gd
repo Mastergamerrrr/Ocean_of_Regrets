8a317d09c210d69a1f6dd2a299981aad4804fcf9
maxlaurencelecena-hub <maxlaurencelecena@gmail.com>
2026-05-13 12:37:07 +0800

    :D 3
    :D 3

--- Scripts/currency.gd
+++ Scripts/currency.gd
@@ -2,6 +2,8 @@ extends CanvasLayer
 
 @onready var label: Label = $MONEY_KWARTA
 
+var force_hidden: bool = false
+
 const ALLOWED_SCENES = [
 	"res://Scenes/game.tscn",
 	"res://Scenes/pond_land.tscn",
@@ -9,7 +11,7 @@ const ALLOWED_SCENES = [
 	"res://Scenes/ocean_land.tscn",
 	"res://Scenes/ocean.tscn",
 	"res://Scenes/lake_land.tscn",
-	"res://Scenes/lake.tscn"
+	"res://Scenes/Lake.tscn"
 ]
 
 func _ready() -> void:
@@ -17,13 +19,15 @@ func _ready() -> void:
 	update_display()
 
 func _process(_delta: float) -> void:
-	_check_visibility()
+	if not force_hidden:
+		_check_visibility()
 
 func _check_visibility() -> void:
 	var current = get_tree().current_scene
 	if current == null or current.scene_file_path == "":
 		visible = false
 		return
+	print("Scene path: ", current.scene_file_path)
 	visible = current.scene_file_path in ALLOWED_SCENES
 
 func update_display() -> void:
